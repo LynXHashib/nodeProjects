f41477aef7dc5360dfdc4eb32e30af32bc8efb2a
LynXHashib <hashib4305@gmail.com>
2025-02-14 15:27:31 +0600

    add cardOverview controller and improve home controller formatting

---
+++ Project2/controller/cardOverview.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+const data
+const cardOverview = (req, res) => {};
--- Project2/controller/home.js
+++ Project2/controller/home.js
@@ -1,9 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 const home = (req, res) => {
+  console.log(req.url);
   res
     .status(200)
-    .sendFile(path.join(__dirname, "..", "public", "home", "app.html"));
+    .sendFile(path.join(__dirname, '..', 'public', 'home', 'app.html'));
 };
 
 module.exports = home;
