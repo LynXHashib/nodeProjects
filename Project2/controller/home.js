e5bfcdc453b783e61bc2acf0364defde93d71be0
LynXHashib <hashib4305@gmail.com>
2025-02-15 04:55:21 +0600

    implement dynamic product card rendering and add template for card overview

--- Project2/controller/home.js
+++ Project2/controller/home.js
@@ -1,10 +1,24 @@
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
+const data = fs.readFileSync(
+  path.join(__dirname, "..", "database", "cards.json"),
+  "utf-8"
+);
+const dataObj = JSON.parse(data);
 
+const homeTemp = fs.readFileSync(
+  path.join(__dirname, "..", "public", "home", "app.html"),
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  path.join(__dirname, "..", "public", "templates", "tempOverview.html"),
+  "utf-8"
+);
 const home = (req, res) => {
   console.log(req.url);
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, '..', 'public', 'home', 'app.html'));
+  const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
+  const output = homeTemp.replace("{%PRODUCT_CARDS%}", cardsHtml);
+  res.status(200).send(output);
 };
 
 module.exports = home;
