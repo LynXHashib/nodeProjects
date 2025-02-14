5fcdd8bca985bd24caca8473d17ac55e8a2cbd4e
LynXHashib <hashib4305@gmail.com>
2025-02-15 05:19:10 +0600

    add card template rendering and styling for product overview

--- Project2/controller/home.js
+++ Project2/controller/home.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+
 const data = fs.readFileSync(
   path.join(__dirname, "..", "database", "cards.json"),
   "utf-8"
@@ -14,6 +15,16 @@ const tempCard = fs.readFileSync(
   path.join(__dirname, "..", "public", "templates", "tempOverview.html"),
   "utf-8"
 );
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRODUCTNAME%}/g, product.name);
+  output = output.replace(/{%QUANTITY%}/g, product.amount);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.id);
+  return output;
+};
+
 const home = (req, res) => {
   console.log(req.url);
   const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
